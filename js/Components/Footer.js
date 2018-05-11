ba92bc3576652c956e8308c24de9bd84325b044d
alienintheheights <alienhard@gmail.com>
2018-05-11 10:30:03 -0500

    typo in footer link

--- js/Components/Footer.js
+++ js/Components/Footer.js
@@ -15,7 +15,7 @@ class Footer extends Component {
             <footer className="col-lg-12 text-center pad-the-top pad-the-bottom">
                 <hr />
                 react :: redux :: php ::
-                website source <a href="https://github.com/alienintheheights/homepage" onClick={this.handleClick.bind(this, 'https://github.com/alienintheheights/homepages')} className="btn-social btn-outline"><span className="sr-only">GitHub</span><i className="fa fa-fw fa-github"></i></a>
+                website source <a href="https://github.com/alienintheheights/homepage" onClick={this.handleClick.bind(this, 'https://github.com/alienintheheights/homepage')} className="btn-social btn-outline"><span className="sr-only">GitHub</span><i className="fa fa-fw fa-github"></i></a>
             </footer>
         )
     }
